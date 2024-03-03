2e06bf7dcca14bd933ee70b0f283a1a513bd2dc7
anirban <codeelephant2@gmail.com>
2024-03-03 10:06:36 +0530

    js file

---
+++ main.js
@@ -0,0 +1,38 @@
+ const body=document.body
+// //body.innerHTML="this is my first html"
+
+ const mydiv=document.createElement('div');
+ mydiv.innerHTML="<strong>hellow this is form div</strong>"
+//  body.appendChild(mydiv)
+// const mdiv=document.querySelectorAll(".cldiv")
+// const mdivArr=Array.from(mdiv)
+// console.log(mdivArr)
+// console.log(mdivArr[0].innerHTML)
+// console.log(mdivArr[1].innerHTML)
+// const body=document.body;
+
+ const child=body.children;
+ console.log(child)
+
+// const childarr=Array.from(child);
+// const selected=childarr[1];
+
+// const parents=selected.children
+// const parentsArr=Array.from(parents)
+// parentsArr[0].setAttribute('id','selected')
+
+// const sib=selected.nextElementSibling;
+// sib.style.backgroundColor='black'
+// sib.previousElementSibling.style.backgroundColor='orange'
+
+const mimage=document.getElementById('mimg')
+mimage.setAttribute('src','flower.jpg')
+
+
+//mchild.closest('.gparent').style.backgroundColor='black'
+
+
+
+// const mydiv=document.getElementById("c1")
+// mydiv.style.backgroundColor='black'
+
